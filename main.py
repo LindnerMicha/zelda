4ab69379eaa6ec8e8481a422fc22d545f1f48001
MethMicha <michael.k.lindner@t-online.de>
2022-10-12 21:15:54 +0200

    Player movement

--- main.py
+++ main.py
@@ -10,7 +10,16 @@ maus_pos = pygame.mouse.get_pos()
 maus_klick = pygame.mouse.get_pressed()                                         #((linke taste, mausrad, rechte taste))
 pixel_font = pygame.font.Font("fonts/PixeloidSans.ttf", 30)
 level_val = 0
-test_k =
+k_up = pygame.K_w
+k_down = pygame.K_s
+k_left = pygame.K_a
+k_right = pygame.K_d
+
+
+playerImg = pygame.image.load("graphics/zelda_test_char.png").convert_alpha()
+playerX = 370
+playerY = 480
+playerSpeed = 5
 
 background = pygame.image.load("graphics/test_bgImg.jpg")
 
@@ -54,11 +63,14 @@ def startscreen():
 def zeichnen():
     pygame.display.update()
 
+
 def level(level_val):
     if level_val == 0:
         background = pygame.image.load("graphics/lvl_text_bg.png")
         screen.blit(background, (0, 0))
 
+def player(playerImg, playerX, playerY):
+    screen.blit(playerImg, (playerX, playerY))
 
 runtime = True
 while runtime:
@@ -67,23 +79,35 @@ while runtime:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        if pressed[pygame.K_ESCAPE]:
+        if pressed[pygame.K_ESCAPE] and option == "Home":
             runtime = False
+        if pressed[pygame.K_ESCAPE] and option != "Home":
+            option = "Home"
+    if pressed[k_up]:
+        playerY -= 1*playerSpeed
+    if pressed[k_down]:
+        playerY += 1*playerSpeed
+    if pressed[k_left]:
+        playerX -= 1*playerSpeed
+    if pressed[k_right]:
+        playerX += 1*playerSpeed
 
     if option == "Home":
         startscreen()
     elif option == "Start":
         level(level_val)
+        player(playerImg, playerX, playerY)
         pass
     elif option == "Credits":
         pass
+
 
 
     zeichnen()
     maus_pos = pygame.mouse.get_pos()
     maus_klick = pygame.mouse.get_pressed()
     clock.tick(fps)
-    pygame.time.wait(10)                        # 10 ms delay (wegen maus)
+    #pygame.time.wait(10)                        # 10 ms delay (wegen maus)
 
 
 
